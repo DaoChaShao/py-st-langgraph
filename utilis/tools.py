eab67c405bbf960e8bbaecf548c9772682eb78e2
DaoChaShao <27882272@qq.com>
2025-02-24 12:28:00 +1300

    feat: add tools.py for sidebar functionality in Streamlit

---
+++ utilis/tools.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3.12
+# -*- Coding: UTF-8 -*-
+# @Time     :   2025/2/24 12:27
+# @Author   :   Shawn
+# @Version  :   Version 0.1.0
+# @File     :   tools.py
+# @Desc     :   
+
+from streamlit import sidebar
